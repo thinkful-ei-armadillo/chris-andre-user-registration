be9f00f5416ff525d3aacd7782c6c4ef653e0d9f
Andre K. W <andre.k.willie@gmail.com>
2019-03-07 15:02:18 -0500

    started working on user insert

--- thingful-server/src/users/users-router.js
+++ thingful-server/src/users/users-router.js
@@ -1,12 +1,15 @@
 'use strict'; 
 
 const express = require('express');
-
+const path = require('path');
 const usersRouter = express.Router(); 
 const jsonBodyParser = express.json(); 
+const UsersService = require('./users-service');
 
 usersRouter
-  .post('/', jsonBodyParser, (req, res) => {
+  .post('/', jsonBodyParser, (req, res, next) => {
+    const { password, user_name } = req.body;
+
     for (const field of ['full_name', 'user_name', 'password']){
       if (!req.body[field]) {
         return res.status(400).json({
@@ -14,7 +17,38 @@ usersRouter
         }); 
       }
     }
-    res.send('ok');
+
+    const passwordError = UsersService.validatePassword(password);
+
+    if(passwordError) {
+      return res.status(400).json({error: passwordError});
+    }
+
+    UsersService.hasUserWithUserName(req.app.get('db'), user_name)
+      .then(hasUser => {
+        if(hasUser) {
+          return res.status(400).json({error: 'Username already taken'})
+        }
+
+        const newUser = {
+          user_name,
+          password,
+          full_name,
+          nickname,
+          date_created: 'now()',
+        };
+
+        return UsersService.insertUser(req.app.get('db'), newUser)
+          .then(user => {
+            res
+              .status(201)
+              .location(path.posix.join(req.originalUrl, `/${user.id}`))
+              .json(UsersService.serializeUser(user))
+          });
+
+        res.send('ok');
+      })
+      .catch(next);
   });
 
 module.exports = usersRouter; 
--- thingful-server/test/users-endpoints.spec.js
+++ thingful-server/test/users-endpoints.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const knex = require('knex');
+const { expect } = require('chai');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
@@ -8,6 +9,7 @@ describe.only('Users Endpoints', function() {
   let db;
 
   const { testUsers } = helpers.makeThingsFixtures();
+  const testUser = testUsers[0];
 
   before('make knex instance', () =>{
     db = knex({
@@ -44,6 +46,125 @@ describe.only('Users Endpoints', function() {
             .post('/api/users')
             .send(registerAttemptBody)
             .expect(400, { error: `Missing '${field}' in request body`});
+        });
+      });
+
+      it(`responds 400 'Password must be longer than 8 characters' when empty password`, () => {
+        const userShortPassword = {
+          user_name: 'test user_name',
+          password: '1234567',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(userShortPassword)
+          .expect(400, {error: 'Password must be longer than 8 characters'});
+      });
+
+      it(`responds 400 'Password must be less than 72' when long password`, () => {
+        const userLongPassword = {
+          user_name: 'test user_name',
+          password: '*'.repeat(73),
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(userLongPassword)
+          .expect(400, {error: 'Password must be less than 72 characters'});
+      });
+
+      it(`responds 400 error when password starts with spaces`, () => {
+        const userPasswordStartsSpaces = {
+          user_name: 'test user_name',
+          password: ' fakepassword!',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(userPasswordStartsSpaces)
+          .expect(400, {error: 'Password must not start or end with empty spaces'});
+      });
+
+      it(`responds 400 error when password ends with spaces`, () => {
+        const userPasswordStartsSpaces = {
+          user_name: 'test user_name',
+          password: 'fakepassword! ',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(userPasswordStartsSpaces)
+          .expect(400, {error: 'Password must not start or end with empty spaces'});
+      });
+
+      it(`responds 400 error when password isn't complex enough`, () => {
+        const userPasswordNotComplex = {
+          user_name: 'test user_name',
+          password: '11AAaabb',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(userPasswordNotComplex)
+          .expect(400, {error: 'Password must contain 1 lowercase, uppercase, number and a special character'});
+      });
+
+      it(`responds 400 'User name already taken' when user_name isn't unique`, () => {
+        const duplicateUser = {
+          user_name: testUser.user_name,
+          password: '11AAaa!!',
+          full_name: 'test full_name',
+        };
+
+        return supertest(app)
+          .post('/api/users')
+          .send(duplicateUser)
+          .expect(400, {error: 'Username already taken'});
+      });
+
+      context(`Happy path`, () => {
+        it(`responds 201, serialized user, storing bcrypted password`, () => {
+          const newUser = {
+            user_name: 'test user_name',
+            password: '11AAaa!!',
+            full_name: 'test full_name',
+          };
+
+          return supertest(app)
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect(res => {
+              expect(res.body).to.have.property('id');
+              expect(res.body.user_name).to.eql(newUser.user_name);
+              expect(res.body.full_name).to.eql(newUser.full_name);
+              expect(res.body.nickname).to.eql('');
+              expect(res.body).to.not.have.property('password');
+              expect(res.headers.location).to.eql(`/api/users/${res.body.id}`);
+              const actualDate = new Date(res.body.date_created).toLocaleString();
+              const expectedDate = new Date().toLocaleString();
+              expect(actualDate).to.eql(expectedDate);
+            })
+            .expect(res =>
+              db
+                .from('thingful_users')  
+                .select('*')
+                .where({id: res.body.id})
+                .first()
+                .then(row => {
+                  expect(row.user_name).to.eql(newUser.user_name);
+                  expect(row.full_name).to.eql(newUser.full_name);
+                  expect(row.nickname).to.eql(null);
+                  const expectedDate = new Date().toLocaleString();
+                  const actualDate = new Date(row.date_created).toLocaleString();
+                  expect(actualDate).to.eql(expectedDate);
+                })
+            );
         });
       });
     });
